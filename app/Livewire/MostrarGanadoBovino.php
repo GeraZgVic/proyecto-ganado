57b291646b04f22809f9faf8ba49957b8e8e2c35
Victor Zúñiga <gerazgvic@gmail.com>
2024-08-22 10:31:26 -0600

    Octava entrega de proyecto

--- app/Livewire/MostrarGanadoBovino.php
+++ app/Livewire/MostrarGanadoBovino.php
@@ -4,6 +4,9 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\GanadoBovino;
+use App\Models\Propietario;
+use App\Models\Razas;
+use App\Models\Sexo;
 use Livewire\WithPagination;
 
 class MostrarGanadoBovino extends Component
@@ -11,20 +14,55 @@ class MostrarGanadoBovino extends Component
     use WithPagination;
 
     public $search = '';
-
+    public $propietario_id = '';
+    public $raza_id = '';
+    public $sexo_id = '';
 
     public function updatingSearch()
     {
         $this->resetPage();
     }
 
-
     public function render()
     {
-        $bovinos = GanadoBovino::orderBy('id_interno', 'asc')->paginate();
+        $query = GanadoBovino::orderBy('id_interno', 'asc');
+
+        // Filtrar por término
+        if ($this->search) {
+            $query->where(function ($q) {
+                $q->where('nombre', 'LIKE', "%" . $this->search . "%")
+                    ->orWhere('id_siniiga', 'LIKE', "%" . $this->search . "%");
+            });
+        }
+
+        // Filtrar por propietario
+        if ($this->propietario_id) {
+            $query->whereHas('propietario', function ($q) {
+                $q->where('id', $this->propietario_id);
+            });
+        }
+
+        // Filtrar por raza
+        if ($this->raza_id) {
+            $query->whereHas('raza', function ($q) {
+                $q->where('id', $this->raza_id);
+            });
+        }
+
+        // Filtrar por sexo
+        if ($this->sexo_id) {
+            $query->whereHas('sexo', function ($q) {
+                $q->where('id', $this->sexo_id);
+            });
+        }
+
+        $bovinos = $query->paginate(10);
 
         return view('livewire.mostrar-ganado-bovino', [
-            'bovinos' => $bovinos
+            'bovinos' => $bovinos,
+            'propietarios' => Propietario::all(),
+            'razas' => Razas::all(),
+            'sexos' => Sexo::all()
         ]);
     }
 }
--- resources/views/livewire/mostrar-ganado-bovino.blade.php
+++ resources/views/livewire/mostrar-ganado-bovino.blade.php
@@ -1,12 +1,53 @@
 <div>
     <x-alerta />
+    <div class="flex flex-col lg:flex-row gap-4 justify-center items-center w-full">
+        <div class="p-1 w-full">
+            {{-- BUSCAR POR TERMINO --}}
+            <input placeholder="Buscar por término"
+                class="w-full p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-md text-sm"
+                id="search" type="text" wire:model.live.debounce.150ms="search">
+        </div>
+
+        <div class="p-1 w-full">
+            {{-- FILTRO POR PROPIETARIO --}}
+            <select wire:model.live.debounce.150ms="propietario_id"
+                class="w-full p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-md text-sm">
+                <option value="">Todos los Propietarios</option>
+                @foreach ($propietarios as $propietario)
+                    <option value="{{ $propietario->id }}">{{ $propietario->nombre }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="p-1 w-full">
+            {{-- FILTRO POR RAZA --}}
+            <select wire:model.live.debounce.150ms="raza_id"
+                class="p-2 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-md text-sm">
+                <option value="">Todas las Razas</option>
+                @foreach ($razas as $raza)
+                    <option value="{{ $raza->id }}">{{ $raza->nombre }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="p-1 w-full">
+            {{-- FILTRO POR SEXO --}}
+            <select wire:model.live.debounce.150ms="sexo_id"
+                class="w-full p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-md text-sm">
+                <option value="">Todos los Sexos</option>
+                @foreach ($sexos as $sexo)
+                    <option value="{{ $sexo->id }}">{{ $sexo->nombre }}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 text-center font-Montserrat">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        
+
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Id Interno
@@ -65,9 +106,10 @@
                                 </svg>
                             </a>
                         </td>
-                        <td scope="row" class="{{getColorById($bovino->id_interno) == 'bg-white' ? 'text-black' : 'text-white'}} whitespace-nowrap">
-                           <span class="{{getColorById($bovino->id_interno)}} px-2 py-1 rounded-full shadow-md">
-                               {{ $bovino->id_interno }}
+                        <td scope="row"
+                            class="{{ getColorById($bovino->id_interno) == 'bg-white' ? 'text-black' : 'text-white' }} whitespace-nowrap">
+                            <span class="{{ getColorById($bovino->id_interno) }} px-2 py-1 rounded-full shadow-md">
+                                {{ $bovino->id_interno }}
                             </span>
                         </td>
                         <td scope="row" class="px-2 py-4  text-gray-500 whitespace-nowrap">
@@ -107,8 +149,8 @@
                             </button>
                         </td>
                     </tr>
+                @endforeach
             </tbody>
-            @endforeach
         </table>
         @if (!$bovinos->count())
             <p class="text-sm my-4 text-gray-500 text-center">No se encuentra ningún registro...</p>
